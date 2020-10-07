fc6fbf49667b844a95a654b4f368df6b3dfe9bf7
GouthamDoddi <gouthambolt@gmail.com>
2020-10-07 13:42:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+
+
+// create a todo
+
+app.post('/todos', async (req, res) => {
+    try {
+
+        const { description } = req.body;
+
+        const newTodo = await pool.query(`INSERT INTO todo (description)
+        VALUES($1)`, [description]
+        );
+
+        res.json(newTodo);
+        
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+
+// get all todo
+
+
+// get a todo
+
+
+//update a todo
+
+
+//delete a todo
+
+
+
+app.listen(5000, () => {
+    console.log('Server is running on 5000');
+});
